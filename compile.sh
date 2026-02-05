682e147f462a1c98e62d4f3b43d0e642022db004
sandorstormen <sandor.berglund@gmail.com>
2026-02-05 11:13:09 +0100

    Remove existing MAR file before recompiling in compile.sh and update the binary file.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -f src/main/resources/org.mal-lang.minCoreLang-1.0.0.mar ]; then
+  rm src/main/resources/org.mal-lang.minCoreLang-1.0.0.mar
+fi
 uv run maltoolbox compile src/main/mal/main.mal langspec.json
 zip -r src/main/resources/org.mal-lang.minCoreLang-1.0.0.mar langspec.json
 rm langspec.json
